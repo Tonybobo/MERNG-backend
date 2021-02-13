c4e6761e271ae0a1ef35e10ce2b1bb93deeea713
Tony Bo <bochuangjie@gmail.com>
2021-02-13 15:09:11 +0800

    deploying server

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,10 @@ const userResolvers = require('./user');
 const commentResolver = require('./comments');
 
 module.exports = {
+	Post: {
+		likeCount: (parent) => parent.likes.length,
+		commentCount: (parent) => parent.comments.length
+	},
 	Query: {
 		...postsResolvers.Query
 	},
--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -1,47 +1,47 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { UserInputError } = require("apollo-server");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
 
-const { SECRET_KEY } = require("../../config");
-const User = require("../../models/User");
+const { SECRET_KEY } = require('../../config');
+const User = require('../../models/User');
 const {
 	validateRegisterInput,
-	validateLoginInput,
-} = require("../../utils/validator");
+	validateLoginInput
+} = require('../../utils/validator');
 
 function generateToken(user) {
 	return jwt.sign(
 		{
 			id: user.id,
 			email: user.email,
-			username: user.username,
+			username: user.username
 		},
 		SECRET_KEY,
-		{ expiresIn: "1h" }
+		{ expiresIn: '1h' }
 	);
 }
 module.exports = {
 	Mutation: {
 		async login(_, { username, password }) {
 			const { errors, valid } = validateLoginInput(username, password);
-			const user = await User.findOne({ username });
 			if (!valid) {
-				throw new UserInputError("Error", { errors });
+				throw new UserInputError('Error', { errors });
 			}
+			const user = await User.findOne({ username });
 			if (!user) {
-				errors.general = "User not found";
-				throw new UserInputError("User not found", { errors });
+				errors.general = 'User not found';
+				throw new UserInputError('User not found', { errors });
 			}
 			const match = await bcrypt.compare(password, user.password);
 			if (!match) {
-				error.general = "Wrong credentail";
-				throw new UserInputError("Wrong credentail", { errors });
+				errors.general = 'Wrong credentail';
+				throw new UserInputError('Wrong credentail', { errors });
 			}
 			const token = generateToken(user);
 			return {
 				...user._doc,
 				id: user._id,
-				token,
+				token
 			};
 		},
 		async register(
@@ -56,15 +56,15 @@ module.exports = {
 				confirmPassword
 			);
 			if (!valid) {
-				throw new UserInputError("Errors", { errors });
+				throw new UserInputError('Errors', { errors });
 			}
 			//TODO: Make sure user doesnt already exist
 			const user = await User.findOne({ username });
 			if (user) {
-				throw new UserInputError("username is taken", {
+				throw new UserInputError('username is taken', {
 					error: {
-						username: " This username is taken",
-					},
+						username: ' This username is taken'
+					}
 				});
 			}
 			//TODO:  Hash password and create token
@@ -74,15 +74,15 @@ module.exports = {
 				email,
 				username,
 				password,
-				createdAt: new Date().toISOString(),
+				createdAt: new Date().toISOString()
 			});
 			const res = await newUser.save();
 			const token = generateToken(res);
 			return {
 				...res._doc,
 				id: res._id,
-				token,
+				token
 			};
-		},
-	},
+		}
+	}
 };
